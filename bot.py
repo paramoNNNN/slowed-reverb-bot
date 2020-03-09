a8fb92758cd07c5c061cb07c0b01a9389277e806
=tahaoj <taha.ojari@gmail.com>
2020-03-09 16:44:24 +0330

    Init commit

---
+++ bot.py
@@ -0,0 +1,42 @@
+import sys
+import time
+import eyed3
+import asyncio
+import telepot
+from librosa import load
+from pysndfx import AudioEffectsChain
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    flavor = telepot.flavor(msg)
+
+    summary = telepot.glance(msg, flavor=flavor)
+    print(flavor, summary)
+    if summary[0] == 'audio':
+      artist = msg['audio']['performer'] if 'performer' in msg['audio'] else 'Unknown Artist'
+      title = msg['audio']['title'] if 'title' in msg['audio'] else 'Untitled (slowed + reverb)'
+      bot.download_file(msg['audio']['file_id'], 'temp/temp.mp3')
+
+      fx = (AudioEffectsChain().reverb().speed(0.7))
+      y, sr = load('temp/temp.mp3')
+      fx(y, 'outputs/temp.mp3')
+      
+      audiofile = eyed3.load('outputs/temp.mp3')
+      audiofile.rename('%s - %s (slowed + reverb)' % (artist, title))
+      audiofile.initTag()
+      audiofile.tag.artist = artist
+      audiofile.tag.title = title
+      audiofile.tag.save()
+
+      audio = open('outputs/%s - %s (slowed + reverb).mp3' % (artist, title), 'rb')
+      bot.sendAudio(summary[2], audio)
+
+
+TOKEN = sys.argv[1]  # get token from command-line
+
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening ...')
+
+while 1:
+    time.sleep(10)
