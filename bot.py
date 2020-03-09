9969588dcf66ec5adb05cde5c6a8d5d0884b4eb9
=tahaoj <taha.ojari@gmail.com>
2020-03-09 17:44:12 +0330

    Add moving files instead of deleting

--- bot.py
+++ bot.py
@@ -20,19 +20,20 @@ def handle(msg):
 
       fx = (AudioEffectsChain().reverb().speed(0.35))
       y, sr = load('temp/temp.mp3')
-      fx(y, 'outputs/temp.mp3')
+      fx(y, 'temp/temp.mp3')
       
-      audiofile = eyed3.load('outputs/temp.mp3')
+      audiofile = eyed3.load('temp/temp.mp3')
       audiofile.rename('%s - %s (slowed + reverb)' % (artist, title))
       audiofile.initTag()
       audiofile.tag.artist = artist
       audiofile.tag.title = title + ' (slowed + reverb)'
       audiofile.tag.save()
+      os.rename('temp/%s - %s (slowed + reverb).mp3' % (artist, title),
+                'outputs/%s - %s (slowed + reverb).mp3' % (artist, title))
 
       audio = open('outputs/%s - %s (slowed + reverb).mp3' % (artist, title), 'rb')
       bot.sendChatAction(summary[2], 'upload_audio')
       bot.sendAudio(summary[2], audio)
-      os.remove('outputs/%s - %s (slowed + reverb).mp3' % (artist, title))
 
 
 TOKEN = sys.argv[1]  # get token from command-line
