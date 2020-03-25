e207462e3cd3970637883e54159797c0a8857713
=tahaoj <taha.ojari@gmail.com>
2020-03-25 15:19:07 +0430

    Add supporting youtube links

--- bot.py
+++ bot.py
@@ -4,15 +4,22 @@ import time
 import shlex
 import eyed3
 import telepot
+import youtube_dl
 from librosa import load
 from subprocess import PIPE, Popen
 from telepot.loop import MessageLoop
 
 def addEffect(audio_file, chat_id):
-  artist = audio_file['performer'] if 'performer' in audio_file else 'Unknown Artist'
-  title = audio_file['title'] if 'title' in audio_file else 'Untitled (slowed + reverb)'
-  bot.sendMessage(chat_id, 'Downloading...')
-  bot.download_file(audio_file['file_id'], 'temp/temp.mp3')
+  if isinstance(audio_file, dict):
+    artist = audio_file['performer'] if 'performer' in audio_file else 'Unknown Artist'
+    title = audio_file['title'] if 'title' in audio_file else 'Untitled (slowed + reverb)'
+    bot.sendMessage(chat_id, 'Downloading...')
+    bot.download_file(audio_file['file_id'], 'temp/temp.mp3')
+  elif isinstance(audio_file, str):
+    meta = audio_file.split('-')
+    artist = meta[0][:-1]
+    title = meta[1][:-4][1:]
+    os.rename(audio_file, 'temp/temp.mp3')
 
   cmd = shlex.split(
       ' '.join([
@@ -47,15 +54,38 @@ def addEffect(audio_file, chat_id):
 
 def handle(msg):
     flavor = telepot.flavor(msg)
-
     summary = telepot.glance(msg, flavor=flavor)
     print(flavor, summary)
+
     if summary[0] == 'audio':
       addEffect(msg['audio'], summary[2])
     elif summary[0] == 'text':
       if '/slowedreverb' in msg['text']:
         if 'reply_to_message' in msg and 'audio' in msg['reply_to_message']:
           addEffect(msg['reply_to_message']['audio'], summary[2])
+      elif msg['text'].startswith('https://www.youtube.com/watch?v='):
+        ydl_opts = {
+          'outtmpl': '%(title)s.%(ext)s',
+          'format': 'bestaudio/best',
+          'postprocessors': [
+            {
+              'key': 'FFmpegExtractAudio',
+              'preferredcodec': 'mp3',
+              'preferredquality': '320',
+            },
+            {
+              'key': 'MetadataFromTitle',
+              'titleformat': '(?P<artist>.*)-(?P<track>.*)',
+            }
+          ],
+          '_titleformat': '(?P<artist>.*)-(?P<track>.*)'
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+          bot.sendMessage(summary[2], 'Downloading...')
+          info = ydl.extract_info(msg['text'], download=True)
+          file_name = ydl.prepare_filename(info)
+          addEffect(file_name.split('.')[0] + '.mp3', summary[2])  
 
 TOKEN = sys.argv[1]  # get token from command-line
 
