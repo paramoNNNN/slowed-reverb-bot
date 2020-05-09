112fd7dbd35276a4a63458856b870cc1407bc38d
taha oj <taha.ojari@gmail.com>
2020-05-09 12:08:44 +0430

    🔥: Add celery to queue add effect tasks, using async and reply to message

--- bot.py
+++ bot.py
@@ -1,13 +1,25 @@
 import os
-import sys
 import time
-import shlex
-import eyed3
 import telepot
-import youtube_dl
-from librosa import load
-from subprocess import PIPE, Popen
-from telepot.loop import MessageLoop
+import asyncio
+from threading import Thread
+from tasks import app, addEffect
+from telepot.aio.loop import MessageLoop
+
+if not os.path.exists('temp'):
+  os.makedirs('temp')
+if not os.path.exists('outputs'):
+  os.makedirs('outputs')
+
+worker_name = ''
+total_workers = 0
+celery_inspect = app.control.inspect()
+celery_stats = celery_inspect.stats()
+if celery_stats:
+  worker_name = list(celery_stats.keys())[0]
+  total_workers = celery_stats[worker_name]['pool']['max-concurrency']
+else:
+  raise Exception('Celery worker\'s not running')
 
 helpMessage = '''
 *Commands*
@@ -40,164 +52,69 @@ shift gives the pitch shift as positive or negative 'cents' (i.e. 100ths of a se
 *Example:* `/tempo 0.9`
 '''
 
-def addEffect(audio_file, chat_id, speed=None, reverb=None, pitch=None, tempo=None):
-  if isinstance(audio_file, dict):
-    artist = audio_file['performer'] if 'performer' in audio_file else 'unknown artist'
-    title = audio_file['title'] if 'title' in audio_file else 'untitled'
-    bot.sendMessage(chat_id, 'Downloading...')
-    bot.download_file(audio_file['file_id'], 'temp/temp.mp3')
-  elif isinstance(audio_file, str):
-    if audio_file.startswith('https://www.youtube.com/watch?v=') or\
-        audio_file.startswith('https://youtube.com/watch?v=') or\
-          audio_file.startswith('https://youtu.be/'):
-      ydl_opts = {
-        'outtmpl': '%(title)s.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [
-          {
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-          },
-          {
-            'key': 'MetadataFromTitle',
-            'titleformat': '(?P<artist>.*)-(?P<track>.*)',
-          }
-        ],
-        '_titleformat': '(?P<artist>.*)-(?P<track>.*)'
-      }
-
-      with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        bot.sendMessage(chat_id, 'Downloading...')
-        info = ydl.extract_info(audio_file, download=True)
-        file_name = ydl.prepare_filename(info)
-        file_name = file_name[:-5]
-        meta = file_name.split('-')
-        if len(meta) > 1:
-          artist = meta[0][:-1]
-          title = meta[1][1:]
-        else:
-          artist = 'unknown artist'
-          title = meta[0]
-        os.rename(file_name + '.mp3', 'temp/temp.mp3')
-    else:
-      meta = audio_file.split('-')
-      if len(meta) > 1:
-        artist = meta[0][:-1]
-        title = meta[1][:-4][1:]
-      else:
-        artist = 'unknown artist'
-        title = meta[0][:-4]
-      os.rename(audio_file, 'temp/temp.mp3')
-  else:
-    return
-
-  commands = []
-  if speed:
-    commands.append('speed')
-    commands.append(speed)
-  if reverb:
-    commands.append('reverb')
-    commands.append(' '.join(reverb))
-  if pitch:
-    commands.append('pitch')
-    commands.append(pitch)
-  if tempo:
-    commands.append('tempo')
-    commands.append(tempo)
-  cmd = shlex.split(
-      ' '.join([
-          'sox',
-          '-N',
-          '-V1',
-          '--ignore-length',
-          'temp/temp.mp3',
-          '-C 320 -r 44100 -b 24 -c 2',
-          'temp/temp2.mp3',
-      ] + list(map(str, commands))),
-      posix=False)
-  bot.sendMessage(chat_id, 'Adding effects...')
-  stdout, stderr = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
-  if stderr:
-      bot.sendMessage(chat_id, 'Something unexpected happend, Try again.')
-      raise RuntimeError(stderr.decode())
-  else:  
-    audiofile = eyed3.load('temp/temp2.mp3')
-    audiofile.initTag()
-    audiofile.tag.artist = artist
-    if speed and reverb:
-      audiofile.tag.artist = artist.lower()
-      audiofile.tag.title = title.lower() + ' ﾉ slowed + reverb ﾉ'
-    elif speed and not reverb and float(speed) < 1:
-      audiofile.tag.artist = artist.lower()
-      audiofile.tag.title = title.lower() + ' ﾉ slowed ﾉ'
-    else:
-      audiofile.tag.title = title
-    audiofile.tag.save()
-    os.rename('temp/temp2.mp3',
-              'outputs/%s - %s.mp3' % (audiofile.tag.artist, audiofile.tag.title))
-
-    audio = open('outputs/%s - %s.mp3' % (audiofile.tag.artist, audiofile.tag.title), 'rb')
-    bot.sendChatAction(chat_id, 'upload_audio')
-    bot.sendAudio(chat_id, audio, performer=audiofile.tag.artist, title=audiofile.tag.title, duration=int(audiofile.info.time_secs))
-
-def parse(reply_to_message, chat_id, speed=None, reverb=None, pitch=None, tempo=None):
+async def parse(reply_to_message, message_id, chat_id, speed=None, reverb=None, pitch=None, tempo=None):
   if 'audio' in reply_to_message:
-    addEffect(reply_to_message['audio'], chat_id, speed=speed, reverb=reverb, pitch=pitch, tempo=tempo)
+    Thread(target=addEffect.delay, args=(reply_to_message['audio'], message_id, chat_id, speed, reverb, pitch, tempo)).start()
   if 'text' in reply_to_message:
-    addEffect(reply_to_message['text'], chat_id, speed=speed, reverb=reverb, pitch=pitch, tempo=tempo)
+    Thread(target=addEffect.delay, args=(reply_to_message['text'], message_id, chat_id, speed, reverb, pitch, tempo)).start()
 
-def handle(msg):
-    flavor = telepot.flavor(msg)
-    summary = telepot.glance(msg, flavor=flavor)
-    print(flavor, summary)
+  active_queues = celery_inspect.active()
+  if len(active_queues[worker_name]) >= total_workers:
+    await bot.sendMessage(chat_id, 'You\'re in queue, please wait...')
 
-    if summary[0] == 'text':
-      if 'reply_to_message' in msg:
-        if '/slowedreverb' in msg['text']:
-          speed = msg['text'].split(' ')
-          if len(speed) > 1:
-            speed = speed[1]
+async def on_chat_message(msg):
+  flavor = telepot.flavor(msg)
+  summary = telepot.glance(msg, flavor=flavor)
+  print(flavor, summary)
+
+  if summary[0] == 'text':
+    if 'reply_to_message' in msg:
+      if '/slowedreverb' in msg['text']:
+        speed = msg['text'].split(' ')
+        if len(speed) > 1:
+          speed = speed[1]
+        else:
+          speed = '0.75'
+        await parse(msg['reply_to_message'], msg['reply_to_message']['message_id'], summary[2], speed=speed, reverb=['50', '50', '100', '100', '20', '0'])
+      elif '/speed' in msg['text']:
+        speed = msg['text'].split(' ')
+        if len(speed) > 1:
+          speed = speed[1]
+        else:
+          speed = '0.75'
+        await parse(msg['reply_to_message'], msg['reply_to_message']['message_id'], summary[2], speed=speed)
+      elif '/reverb' in msg['text']:
+        reverb = msg['text'].split(' ')
+        if len(reverb) == 1:
+          reverb = ['50', '50', '100', '100', '20', '0']
+        else:
+          reverb.pop(0)
+        await parse(msg['reply_to_message'], msg['reply_to_message']['message_id'], summary[2], reverb=reverb)
+      elif '/pitch' in msg['text']:
+          pitch = msg['text'].split(' ')
+          if len(pitch) > 1:
+            pitch = pitch[1]
           else:
-            speed = '0.75'
-          parse(msg['reply_to_message'], summary[2], speed=speed, reverb=['50', '50', '100', '100', '20', '0'])
-        elif '/speed' in msg['text']:
-          speed = msg['text'].split(' ')
-          if len(speed) > 1:
-            speed = speed[1]
+            pitch = '500'
+          await parse(msg['reply_to_message'], msg['reply_to_message']['message_id'], summary[2], pitch=pitch)
+      elif '/tempo' in msg['text']:
+          tempo = msg['text'].split(' ')
+          if len(tempo) > 1:
+            tempo = tempo[1]
           else:
-            speed = '0.75'
-          parse(msg['reply_to_message'], summary[2], speed=speed)
-        elif '/reverb' in msg['text']:
-          reverb = msg['text'].split(' ')
-          if len(reverb) == 1:
-            reverb = ['50', '50', '100', '100', '20', '0']
-          else:
-            reverb.pop(0)
-          parse(msg['reply_to_message'], summary[2], reverb=reverb)
-        elif '/pitch' in msg['text']:
-            pitch = msg['text'].split(' ')
-            if len(pitch) > 1:
-              pitch = pitch[1]
-            else:
-              pitch = '500'
-            parse(msg['reply_to_message'], summary[2], pitch=pitch)
-        elif '/tempo' in msg['text']:
-            tempo = msg['text'].split(' ')
-            if len(tempo) > 1:
-              tempo = tempo[1]
-            else:
-              tempo = '0.8'
-            parse(msg['reply_to_message'], summary[2], tempo=tempo)
-      elif '/start' in msg['text'] or '/help' in msg['text']:
-        bot.sendMessage(summary[2], helpMessage, parse_mode='Markdown')
+            tempo = '0.8'
+          await parse(msg['reply_to_message'], msg['reply_to_message']['message_id'], summary[2], tempo=tempo)
+    elif '/start' in msg['text'] or '/help' in msg['text']:
+      await bot.sendMessage(summary[2], helpMessage, parse_mode='Markdown')
 
+TOKEN = open('token', 'r').read()  # get token from the token file
 
-TOKEN = sys.argv[1]  # get token from command-line
+bot = telepot.aio.Bot(TOKEN)
+answerer = telepot.aio.helper.Answerer(bot)
 
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
+loop = asyncio.get_event_loop()
+loop.create_task(MessageLoop(bot, {'chat': on_chat_message}).run_forever())
+
 print ('Listening ...')
 
-while 1:
-    time.sleep(10)
+loop.run_forever()
---
+++ tasks.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import shlex
+import eyed3
+import telepot
+import youtube_dl
+from celery import Celery
+from datetime import datetime
+from subprocess import PIPE, Popen
+
+app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
+
+
+TOKEN = open('token', 'r').read()
+bot = telepot.Bot(TOKEN)
+
+def send_message(chat_id, message_id, text):
+  try:
+    bot.sendMessage(chat_id, reply_to_message_id=message_id, text=text)
+  except:
+    bot.sendMessage(chat_id, text=text)
+
+@app.task
+def addEffect(audio_file, message_id, chat_id, speed=None, reverb=None, pitch=None, tempo=None):
+  date_time = datetime.strftime(datetime.utcnow(), '%s')
+  file_name = ''
+  if isinstance(audio_file, dict):
+    artist = audio_file['performer'] if 'performer' in audio_file else 'unknown artist'
+    title = audio_file['title'] if 'title' in audio_file else 'untitled'
+    send_message(chat_id, message_id, 'Downloading...')
+    bot.download_file(audio_file['file_id'], f'temp/temp_{date_time}.mp3')
+  elif isinstance(audio_file, str):
+    if audio_file.startswith('https://www.youtube.com/watch?v=') or\
+        audio_file.startswith('https://youtube.com/watch?v=') or\
+          audio_file.startswith('https://youtu.be/'):
+      ydl_opts = {
+        'outtmpl': '%(title)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [
+          {
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+          },
+          {
+            'key': 'MetadataFromTitle',
+            'titleformat': '(?P<artist>.*)-(?P<track>.*)',
+          }
+        ],
+        '_titleformat': '(?P<artist>.*)-(?P<track>.*)'
+      }
+
+      with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        send_message(chat_id, message_id, 'Downloading...')
+        info = ydl.extract_info(audio_file, download=True)
+        file_name = ydl.prepare_filename(info)
+        file_name = file_name[:-5]
+        meta = file_name.split('-')
+        if len(meta) > 1:
+          artist = meta[0][:-1]
+          title = meta[1][1:]
+        else:
+          artist = 'unknown artist'
+          title = meta[0]
+        os.rename(f'{file_name}.mp3', f'temp/temp_{date_time}.mp3')
+    else:
+      file_name = audio_file
+      meta = audio_file.split('-')
+      if len(meta) > 1:
+        artist = meta[0][:-1]
+        title = meta[1][:-4][1:]
+      else:
+        artist = 'Unknown Artist'
+        title = meta[0][:-4]
+      os.rename(file_name, f'temp/temp_{date_time}.mp3')
+  else:
+    return
+
+  commands = []
+  if speed:
+    commands.append('speed')
+    commands.append(speed)
+  if reverb:
+    commands.append('reverb')
+    commands.append(' '.join(reverb))
+  if pitch:
+    commands.append('pitch')
+    commands.append(pitch)
+  if tempo:
+    commands.append('tempo')
+    commands.append(tempo)
+  cmd = shlex.split(
+      ' '.join([
+          'sox',
+          '-N',
+          '-V1',
+          '--ignore-length',
+          '-G',
+          f'temp/temp_{date_time}.mp3',
+          '-C 320 -r 44100 -b 24 -c 2',
+          f'temp/temp_{date_time}.tmp.mp3',
+      ] + list(map(str, commands))),
+      posix=False)
+  send_message(chat_id, message_id, 'Adding effects...')
+  stdout, stderr = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
+  if stderr:
+    send_message(chat_id, message_id, 'Something unexpected happend, Try again.')
+    raise RuntimeError(stderr.decode())
+  else:  
+    audiofile = eyed3.load(f'temp/temp_{date_time}.tmp.mp3')
+    audiofile.initTag()
+    audiofile.tag.artist = artist
+    if speed and reverb:
+      audiofile.tag.artist = artist.lower()
+      audiofile.tag.title = title.lower() + ' ﾉ slowed + reverb ﾉ'
+    elif speed and not reverb and float(speed) < 1:
+      audiofile.tag.artist = artist.lower()
+      audiofile.tag.title = title.lower() + ' ﾉ slowed ﾉ'
+    else:
+      audiofile.tag.title = title
+    audiofile.tag.save()
+    os.rename(f'temp/temp_{date_time}.tmp.mp3',
+              'outputs/%s - %s.mp3' % (audiofile.tag.artist, audiofile.tag.title))
+
+    audio = open('outputs/%s - %s.mp3' % (audiofile.tag.artist, audiofile.tag.title), 'rb')
+    bot.sendChatAction(chat_id, 'upload_audio')
+    bot.sendAudio(chat_id, audio, performer=audiofile.tag.artist, title=audiofile.tag.title, duration=int(audiofile.info.time_secs))
