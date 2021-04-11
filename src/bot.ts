c4e9c191eaf9fe02bd0bb63ac9a07f31c6d96918
Taha <taha.ojari@gmail.com>
2021-04-11 17:19:10 +0430

    feat: rewrite app in node.js/typescript

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,8 @@
-import { Telegraf } from 'telegraf'
-const stream = require('stream');
-const {promisify} = require('util');
-import { createWriteStream } from 'fs'
-import got from 'got'
-import dotenv from 'dotenv'
+import { Telegraf } from "telegraf";
+import { addEffectQueue } from "./queues";
+import dotenv from "dotenv";
 
-dotenv.config()
-const pipeline = promisify(stream.pipeline);
+dotenv.config();
 
 const helpMessage = `
 <b>Commands</b>
@@ -37,13 +33,84 @@ Change the audio playback speed but not its pitch.
 shift gives the pitch shift as positive or negative 'cents' (i.e. 100ths of a semitone)
 <b>Default value if none given:</b> 0.8
 <b>Example:</b> <code>/tempo 0.9</code>
-`
+`;
 
-const bot: Telegraf = new Telegraf(process.env.TOKEN)
+const bot: Telegraf = new Telegraf(process.env.TOKEN);
 
-bot.on('text', (ctx) => {
-  ctx.replyWithHTML('Bot under contruction... \n <b>@paramoNNNN</b> ')
-})
+bot.start((ctx) => ctx.replyWithHTML(helpMessage));
+bot.help((ctx) => ctx.replyWithHTML(helpMessage));
 
-console.log('Starting slowed+reverb bot')
-bot.launch()
+// TODO: Set a proper type to ctx
+const checkMessage = (ctx: any) =>
+  ctx.update.message.reply_to_message &&
+  "audio" in ctx.update.message.reply_to_message;
+
+// TODO: Set a proper type to ctx
+const splitMessage = (ctx: any, defaultValue: string | string[]) => {
+  const message = ctx.update.message.text.split(" ");
+  return message.length > 1 ? message[1] : defaultValue;
+};
+
+// TODO: Set a proper type to ctx
+const addQueue = (
+  ctx: any,
+  speed?: string,
+  reverb?: string[],
+  pitch?: string,
+  tempo?: string
+) => {
+  const message = ctx.update.message.reply_to_message;
+  if (message && "audio" in message) {
+    addEffectQueue.add("process", {
+      audio: message.audio,
+      messageId: ctx.message.message_id,
+      chatId: ctx.chat.id,
+      speed,
+      reverb,
+      pitch,
+      tempo,
+    });
+  }
+};
+
+bot
+  .command("slowedreverb", (ctx) => {
+    if (checkMessage(ctx)) {
+      const speed = splitMessage(ctx, "0.75");
+      addQueue(ctx, speed, ["50", "50", "100", "100", "20", "0"]);
+    }
+  })
+  .catch(() => {});
+
+bot.command("speed", (ctx) => {
+  if (checkMessage(ctx)) {
+    const speed = splitMessage(ctx, "0.75");
+    addQueue(ctx, speed);
+  }
+});
+
+bot.command("reverb", (ctx) => {
+  if (checkMessage(ctx)) {
+    let reverb = ctx.update.message.text.split(" ");
+    if (reverb.length > 0) reverb.shift();
+    else reverb = ["50", "50", "100", "100", "20", "0"];
+    addQueue(ctx, undefined, reverb);
+  }
+});
+
+bot.command("pitch", (ctx) => {
+  if (checkMessage(ctx)) {
+    const pitch = splitMessage(ctx, "500");
+    addQueue(ctx, undefined, undefined, pitch);
+  }
+});
+
+bot.command("tempo", (ctx) => {
+  if (checkMessage(ctx)) {
+    const tempo = splitMessage(ctx, "0.8");
+    addQueue(ctx, undefined, undefined, undefined, tempo);
+  }
+});
+
+console.log("Starting slowed+reverb bot");
+bot.launch();
